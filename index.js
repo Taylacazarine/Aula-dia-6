d6f67291f38dc64f0ba31cc624012811ae0a57a8
Tayla Cazarine <taylacazarine@gmail.com>
2026-03-06 09:39:33 -0300

    projeto express aula passada

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");     //A linha 1 e 2, cria o servidor. Linha 1  - está IMPORTANDO o Express, Linha 2 - Você está EXECUTANDO o Express: Isso cria o servidor.
+const servidor = express();
+
+servidor.get ('/pessoa/:nome/:idade', (req,res) =>{
+
+        const nome = req.params.nome 
+        const idade = req.params.idade
+        res.send(`Seu nome é ${nome} e sua idade é ${idade} anos.`)
+
+})
+const porta = 3026;
+
+servidor.listen(porta, () => {
+  console.log("Afonso servidor está ligado");
+});
+
+
